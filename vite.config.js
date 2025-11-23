43fdfd9f405e9578b8e4e7faebf9de396b6766d5
Ali Reda <alyredagomaa@gmail.com>
2025-11-23 12:47:44 +0200

    fix vite & rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "", 
+  base: "./", // use relative paths for JS/CSS
   build: {
-    outDir: "dist",
+    outDir: "dist", // output folder
+    emptyOutDir: true, // clean dist before build
   },
 });
